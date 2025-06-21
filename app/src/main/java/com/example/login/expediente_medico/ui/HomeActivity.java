bfd19999c0794dc1e5c220fd84a79c4f747b27be
Fernando Barraza <fernando.jba.2000@gmail.com>
2025-06-20 20:39:59 -0600

    Cambios UI

--- app/src/main/java/com/example/login/expediente_medico/ui/HomeActivity.java
+++ app/src/main/java/com/example/login/expediente_medico/ui/HomeActivity.java
@@ -14,27 +14,27 @@ public class HomeActivity extends AppCompatActivity{
         setContentView(R.layout.activity_home);
 
         // Botón que abre la lista de doctores
-        findViewById(R.id.btnDoctores).setOnClickListener(v -> {
+        findViewById(R.id.btnIrDoctores).setOnClickListener(v -> {
             startActivity(new Intent(this, DoctoresActivity.class));
         });
 
         // Botón que abre la lista de pacientes
-        findViewById(R.id.btnPacientes).setOnClickListener(v -> {
+        findViewById(R.id.btnIrPacientes).setOnClickListener(v -> {
             startActivity(new Intent(this, PacientesActivity.class));
         });
 
         // Botón que abre la lista de especialidades
-        findViewById(R.id.btnEspecialidades).setOnClickListener(v ->
+        findViewById(R.id.btnIrEspecialidades).setOnClickListener(v ->
                 startActivity(new Intent(this, EspecialidadesActivity.class))
         );
 
         // Botón que abre la lista de citas
-        findViewById(R.id.btnCitas).setOnClickListener(v ->
+        findViewById(R.id.btnIrCitas).setOnClickListener(v ->
                 startActivity(new Intent(this, CitasActivity.class))
         );
 
         // Botón que abre la lista de registro/historial
-        findViewById(R.id.btnExpediente).setOnClickListener(v ->
+        findViewById(R.id.btnIrExpediente).setOnClickListener(v ->
                 startActivity(new Intent(this, ExpedienteActivity.class))
         );
 
